8d65578121fb9217a6dd9be553bc7d97355cc0a7
toduco5 <tminhduc1304@gmail.com>
2026-05-23 09:59:22 +0700

    fisrt time

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hello world & hoidanit</h1>
+      <h1>Chào anh Tô Đức</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
