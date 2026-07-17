9ee8b218497610ef5b2adb8ca9a01b52fea538df
To Minh Duc <dellcanbiet2@gmail.com>
2026-07-17 13:23:05 +0700

    vd45

--- src/assets/style/TodoContent.jsx
+++ src/assets/style/TodoContent.jsx
@@ -1,5 +1,5 @@
 const Todo_Content = (props) => {
-    const { todoList,deleteTodo } = props
+    const { todoList, deleteTodo } = props
     // console.log("Todo_Content ", props)
     // const todoList = props.todoList
     console.log("in ra", todoList)// kế thừa component todolist từ app.jsx(props)
@@ -9,31 +9,18 @@ const Todo_Content = (props) => {
     return (
         <div className="todo-content">
             {todoList.map((item, index) => {
-                {console.log("check map: ", item, index)}
+                { console.log("check map: ", item, index) }
                 return (
                     <div className="btn-todo-content" key={item.id}>
                         <div>
                             {item.id}.{item.name}
                         </div>
                         <button
-                            onClick={()=>handleClickDelete(item.id)}
+                            onClick={() => handleClickDelete(item.id)}
                         >delete</button>
                     </div>
                 )
             })}
-            {
-                console.log("check item: ",)
-            /* <p>
-                My name is {props.name} and I'm {props.age} years old, form {props.data.address} - {props.data.country} <hr />
-            </p> */}
-            <div
-                style={{
-                    color: "red",
-                    fontSize: "20px",
-                    fontWeight: "bold"
-                }}>
-                {/* {JSON.stringify(props.todoList)} */}
-            </div>
         </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './assets/style/Mycomponent.css'
-
 import './assets/style/todolist.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path:"/login",
+    element: <>Login Page</>
+  },
+  {
+    path: "/register",
+    element: <>Register Page</>
+  },
+  {
+    path: "/contact",
+    element: <>Contact Page</>
+  },
+  {
+    path: "/User",
+    element: <>User Page</>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
